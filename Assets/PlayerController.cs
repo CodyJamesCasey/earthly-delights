c1f96ab0b03ceb02ce79b22a1492fa4db2d85f44
Cody Casey <codyjamescasey@gmail.com>
2016-12-07 20:22:34 -0500

    first shot at sizing the player and arrow game objects

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+  public float speed;
+  public float tilt;
+
+  public GameObject arrow;
+  public Transform shotSpawn;
+  public float fireRate;
+
+  private float nextFire;
+
+  void Update ()
+  {
+    if (Input.GetButton("Fire1") && Time.time > nextFire) 
+    {
+      nextFire = Time.time + fireRate;
+      Instantiate(arrow, shotSpawn.position, shotSpawn.rotation);
+      GetComponent<AudioSource>().Play ();
+    }
+  }
+}
