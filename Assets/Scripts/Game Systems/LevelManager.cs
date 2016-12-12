848ee2b149dfa7f83287cd7c4e2ec65fd2f102b9
Greg Johnson <greg93John@gmail.com>
2016-12-11 19:31:36 -0500

    Adds Start Menu Scene (with additional Content)
    Adds a new scene "Start_Menu", with bare-minimum Canvas setup in the
    scene.
    
    adds "Font" folder in the Asset folder, with multiple imported fonts.
    
    adds Prefab GameObject of "Level Manager" in "Game Systems" sub folder
    in Prefabs folder.
    
    adds Script "LevelManager" in "Game Systems" sub folder in Scripts
    folder.

---
+++ Assets/Scripts/Game Systems/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    private string nameHolder;
+
+    // Loading levels incremented in listing order.
+    public void LoadNextLevelDelay(float delay) {
+        Invoke("LoadNextLevel", delay);
+    }
+
+    public void LoadNextLevel() {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    // Loading levels by name.
+    public void LoadLevelByNameDelay(string name, float delay) {
+        nameHolder = name;
+        Invoke("LoadLevelByNameCall", delay);
+    }
+
+    private void LoadLevelByNameCall() {
+        LoadLevelByName(nameHolder);
+        nameHolder = null;
+    }
+
+    public void LoadLevelByName(string name) {
+		Debug.Log ("New Level load: " + name);
+        SceneManager.LoadScene(name);
+	}
+
+    // Quiting the game.
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}
