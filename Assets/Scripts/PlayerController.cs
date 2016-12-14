79c777308073ef7a92acf269a19d36726d07baaa
Greg Johnson <greg93John@gmail.com>
2016-12-14 02:00:49 -0500

    Modifies PlayerController.cs
    Updates PlayerControler script to allow changing of pivot direction, and
    stopping the pivot mvement on firing an arrow.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,21 +16,24 @@ public class PlayerController : MonoBehaviour
   public Transform shotSpawn;
   public float fireRate;
 
-  private float nextFire, pivotDirection = 1f;;
+  private float nextFire, pivotDirection = 1f;
 
   void Update ()
   {
-    transform.RotateAround(Vector3.zero, Vector3.up*pivotDirection, pivotSpeed * Time.deltaTime);
-    if (Input.GetButton("Fire1") && Time.time > nextFire)
-    {
-      nextFire = Time.time + fireRate;
-      GameObject projectile = Instantiate(arrow, shotSpawn.position, shotSpawn.rotation) as GameObject;
+        if (Time.time > nextFire) {
+            shotSpawn.RotateAround(transform.position, Vector3.forward * pivotDirection, pivotSpeed * Time.deltaTime);
+            if (shotSpawn.localPosition.x <= 0.2f) { ChangePivotDirection(); }
 
-      // assigns the newly Instantiated arrow GameObject to be a child of the "Shot Spawn" GameObject
-      projectile.transform.parent = shotSpawn;
+            if (Input.GetButton("Fire1")) {
+                nextFire = Time.time + fireRate;
+                GameObject projectile = Instantiate(arrow, shotSpawn.position, shotSpawn.rotation) as GameObject;
 
-      LaunchArrow(projectile);
-    }
+                // assigns the newly Instantiated arrow GameObject to be a child of the "Shot Spawn" GameObject
+                projectile.transform.parent = shotSpawn;
+
+                LaunchArrow(projectile);
+            }
+        }
   }
     // Function Launches the arrow in the direction of the 'X' axis, relative to the "Shot Spawn" GameObject
     void LaunchArrow (GameObject launchingArrow)
